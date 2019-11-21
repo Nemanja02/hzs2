f1556998566403c2d1e16dee74dd55693be20dfc
Vodnik <petar.radojevic22@gmail.com>
2019-11-21 23:14:02 +0100

    lol

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <!DOCTYPE html>
 <html lang="sr">
 
-<head>
-  <link rel="stylesheet" href="/styles/hamburgers.css" />
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles/global.css">
-  <title>xD</title>
-  <link rel="stylesheet" href="styles/index/index.css">
-</head>
+<?php echo file_get_contents('components/head/head.html'); ?>
 
 <body>
   <?php echo file_get_contents('components/navigation/navigation.html'); ?>
