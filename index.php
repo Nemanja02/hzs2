db62aef9ceeb431416c31316632eb3af6cc25a41
Vodnik <petar.radojevic22@gmail.com>
2019-11-22 00:10:28 +0100

    jeste

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
 
 <body>
   <?php echo file_get_contents('components/navigation/navigation.html'); ?>
-  <div class="banner"></div>
+  <div class="banner">
+    <div>
+      <h1>Najbolja zabava u gradu</h1>
+      <h2>Sve na jednom mestu</h2>
+    </div>
+  </div>
   <div class="float"></div>
 </body>
 
