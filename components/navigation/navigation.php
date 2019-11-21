983ffec945ed5f4fb29278512b6fe389fb5568ea
Vodnik <petar.radojevic22@gmail.com>
2019-11-21 20:36:09 +0100

    Nav beggining

--- components/navigation/navigation.php
+++ components/navigation/navigation.php
@@ -1,6 +1,12 @@
-<link rel="stylesheet" href="components/navigation/navigation.css">
+<link rel="stylesheet" href="/components/navigation/navigation.css">
 <nav>
   <ul>
-
+    <li><a href="/">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">123</a></li>
+    <li><a href="#">123</a></li>
+    <li><a href="#">123</a></li>
+    <li><a href="#">123</a></li>
+    <li><a href="#">123</a></li>
   </ul>
 </nav>
--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="sr">
+
 <head>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="styles/global.css">
   <title>xD</title>
 </head>
+
 <body>
   <?php echo file_get_contents('components/navigation/navigation.php'); ?>
 </body>
+
 </html>
