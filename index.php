36953e9612e86847b07351eddb9b6e197734efce
Vodnik <petar.radojevic22@gmail.com>
2019-11-21 20:52:12 +0100

    nav placeholder

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -11,7 +12,8 @@
 </head>
 
 <body>
-  <?php echo file_get_contents('components/navigation/navigation.php'); ?>
+  <?php echo file_get_contents('components/navigation/navigation.html'); ?>
+
 </body>
 
 </html>
