131f731799a3605d34c1043fa52eacea5d248969
Vodnik <petar.radojevic22@gmail.com>
2019-11-21 20:08:54 +0100

    Project structure beggining

---
+++ components/navigation/navigation.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="components/navigation/navigation.css">
+<nav>
+  <ul>
+
+  </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="sr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styles/global.css">
+  <title>xD</title>
+</head>
+<body>
+  <?php echo file_get_contents('components/navigation/navigation.php'); ?>
+</body>
+</html>
